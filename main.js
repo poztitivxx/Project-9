094636bf2e9d870ff285070a7a0482e16613c2ef
poztitivxx <127322573+poztitivxx@users.noreply.github.com>
2023-03-18 14:03:38 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+let calculate = document.getElementById("calculation");
+let count = document.getElementById("buttonCountNumber");
+calculation = document.getElementById("calculation").innerHTML;
+
+document.getElementById("buttonCountPlus").onclick = function() {
+  let countPlus = count.innerHTML;
+  if(+countPlus <= 5){
+    count.innerHTML++;
+    let countPlus = count.innerHTML;
+    calculate.innerHTML = calculations(countPlus) ;
+  }
+}
+
+document.getElementById("buttonCountMinus").onclick = function() {
+  let countMinus = count.innerHTML;
+  if(+countMinus >= 2){
+    count.innerHTML--;
+    let countMinus = count.innerHTML;
+    calculate.innerHTML = calculations(countMinus) ;
+  }
+}
+
+calculations = (count) => {
+  return  +(count)*(+calculation);
+}
+
